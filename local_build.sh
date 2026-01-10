de8086673c72a3410cfd69958f3241a7c2c274cc
dejanu <dejanualexandru@gmail.com>
2026-01-10 19:34:49 +0200

    pack LICENSE

--- local_build.sh
+++ local_build.sh
@@ -3,6 +3,8 @@
 # set vars
 VERSION=v1.0.1
 BIN=kubectl-imagescan
+LICENSE=LICENSE
+
 # build flags: strip symbols to reduce size
 LDFLAGS="-s -w"
 
@@ -13,28 +15,28 @@ rm -f ./*-${VERSION}-*.tar.gz || true
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} .
 
-tar czf ${BIN}-${VERSION}-darwin-arm64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-darwin-arm64.tar.gz ${BIN} ${LICENSE}
 rm ${BIN}
 
 # macOS amd64
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} .
 
-tar czf ${BIN}-${VERSION}-darwin-amd64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-darwin-amd64.tar.gz ${BIN} ${LICENSE}
 rm ${BIN}
 
 # Linux arm64
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} .
 
-tar czf ${BIN}-${VERSION}-linux-arm64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-linux-arm64.tar.gz ${BIN} ${LICENSE}
 rm ${BIN}
 
 # Linux amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} 
 
-tar czf ${BIN}-${VERSION}-linux-amd64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-linux-amd64.tar.gz ${BIN} ${LICENSE}
 rm ${BIN}
 
 # compute sha for each artifact
