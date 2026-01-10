5423d49fa19a3f13b44ba87cfc503439220e8baa
dejanu <dejanualexandru@gmail.com>
2026-01-10 17:38:55 +0200

    rebuild artifacts

--- local_build.sh
+++ local_build.sh
@@ -7,32 +7,37 @@ BIN=kubectl-imagescan
 LDFLAGS="-s -w"
 
 # clean old artifacts
-rm -f ./*_${VERSION}_*.tar.gz || true
+rm -f ./*-${VERSION}-*.tar.gz || true
 
 # macOS arm64
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} .
 
-tar czf ${BIN}_${VERSION}_darwin_arm64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-darwin-arm64.tar.gz ${BIN}
 rm ${BIN}
 
 # macOS amd64
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} .
 
-tar czf ${BIN}_${VERSION}_darwin_amd64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-darwin-amd64.tar.gz ${BIN}
 rm ${BIN}
 
 # Linux arm64
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} .
 
-tar czf ${BIN}_${VERSION}_linux_arm64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-linux-arm64.tar.gz ${BIN}
 rm ${BIN}
 
 # Linux amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
 go build -ldflags="$LDFLAGS" -o ${BIN} 
 
-tar czf ${BIN}_${VERSION}_linux_amd64.tar.gz ${BIN}
+tar czf ${BIN}-${VERSION}-linux-amd64.tar.gz ${BIN}
 rm ${BIN}
+
+# compute sha for each artifact
+for file in ./*-${VERSION}-*.tar.gz; do
+  shasum -a 256 $file 
+done
